49dc6a54d025a293c52d215141ba6f71814cb705
stoikal <laksito@protonmail.com>
2025-02-23 01:28:50 +0700

    util: merge polygons into multipolygon

---
+++ polygonsToMultiPolygon.js
@@ -0,0 +1,30 @@
+import fs from "fs";
+import { features } from "process";
+
+const path = "./provinces/sumut.json"
+
+const jsonString = fs.readFileSync(path, 'utf8');
+const json = JSON.parse(jsonString);
+
+const polygonFeatures = json.features;
+
+const multiPolygonGeometry = {
+  type: "MultiPolygon",
+  coordinates: polygonFeatures.map((feature) => {
+    return feature.geometry.coordinates
+  })
+}
+
+const multiPolygonFeature = {
+  ...polygonFeatures[0],
+  geometry: multiPolygonGeometry,
+}
+
+const featureCollection = {
+  ...json,
+  features: [multiPolygonFeature]
+}
+
+const resultName = path.replace(/\.json$/, "-multi.json");
+
+fs.writeFileSync(resultName, JSON.stringify(featureCollection))
