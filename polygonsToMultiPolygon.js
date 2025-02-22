44aca6f0870150f096a20c72b53ad9b43b26f629
stoikal <laksito@protonmail.com>
2025-02-23 02:44:39 +0700

    util: simplify

--- polygonsToMultiPolygon.js
+++ polygonsToMultiPolygon.js
@@ -1,5 +1,4 @@
 import fs from "fs";
-import { features } from "process";
 
 const path = "./provinces/sumut.json"
 
---
+++ simplify.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+import simplify from "simplify-geojson";
+
+const path = "./provinces/sumatra.json"
+
+const jsonString = fs.readFileSync(path, 'utf8');
+const json = JSON.parse(jsonString);
+
+
+var simplified = simplify(json, 0.01)
+
+
+const resultName = path.replace(/\.json$/, "-simplified.json");
+
+fs.writeFileSync(resultName, JSON.stringify(simplified))
