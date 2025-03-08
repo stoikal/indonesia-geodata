a6e9c0c762bd0f1e7a8893eb2cbac01d93df467d
stoikal <laksito@protonmail.com>
2025-03-08 16:42:29 +0700

    build: restructure folders

--- buildJson.js
+++
@@ -1,4 +0,0 @@
-import fs from "fs";
-import map from "./indonesiaLow.js";
-
-fs.writeFileSync("output.json", JSON.stringify(map))
--- indonesiaLow.d.ts
+++
@@ -1,3 +0,0 @@
-declare const map: GeoJSON.FeatureCollection;
-
-export default map;
